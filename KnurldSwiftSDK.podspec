3c9e378ccf0875ffc87e0d30c552fd1f131ca26a
Ryan Conway <rconway@knurld.com>
2016-07-07 12:19:18 -0700

    Add missing files

---
+++ KnurldSwiftSDK.podspec
@@ -0,0 +1,17 @@
+Pod::Spec.new do |spec|
+  spec.name = "KnurldSwiftSDK"
+  spec.version = "1.0.0"
+  spec.summary = "Knurld Swift SDK."
+  spec.homepage = "https://github.com/pumpmagic/SwiftSDK"
+  spec.license = { type: 'Intellisis', file: 'LICENSE' }
+  spec.authors = { "Ryan Conway" => 'rconway@knurld.com' }
+  spec.social_media_url = "http://twitter.com/knurld"
+
+  spec.platform = :ios, "9.1"
+  spec.requires_arc = true
+  spec.source = { git: "https://github.com/pumpmagic/SwiftSDK.git", tag: "v#{spec.version}", submodules: true }
+  spec.source_files = "SwiftSDK/**/*.{h,swift}"
+
+  spec.dependency "Alamofire", "~> 3.4"
+  spec.dependency "SwiftyJSON"
+end
---
+++ SwiftSDKTests/SwiftSDKTests.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftSDKTests.swift
+//  SwiftSDKTests
+//
+//  Created by Ryan Conway on 7/7/16.
+//  Copyright © 2016 Knurld. All rights reserved.
+//
+
+import XCTest
+import SwiftSDK
+
+class SwiftSDKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        //@todo actually implement an asynchronous test; for now, this just verifies linking
+        authorize(clientID: TEST_CLIENT_ID, clientSecret: TEST_CLIENT_SECRET,
+                  onSuccess: { accessToken in print("Application got token: \(accessToken)") },
+                  onFailure: { error in print("Application got error: \(error)") })
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
