e24acc6cd9d2aca205c41426c7a1efc39f4b082e
Ryan Conway <rconway@knurld.com>
2016-07-07 14:28:26 -0700

    Add dummy test credentials

---
+++ SwiftSDKTests/TestCredentials.swift
@@ -0,0 +1,15 @@
+//
+//  TestCredentials.swift
+//  SwiftSDK
+//
+//  Created by Ryan Conway on 7/6/16.
+//  Copyright © 2016 Knurld. All rights reserved.
+//
+
+import Foundation
+
+
+// Fill in your own credentials here
+let TEST_DEVELOPER_ID = "Bearer: blahblahblah"
+let TEST_CLIENT_ID = "blahblahblahblahblahblah"
+let TEST_CLIENT_SECRET = "blahblahblah"
