bca39e73902f30b1501bfeca185679810112f7af
Ryan Conway <rconway@knurld.com>
2016-07-07 14:22:48 -0700

    Move to Quick and Nimble for tests

--- SwiftSDKTests/SwiftSDKTests.swift
+++ SwiftSDKTests/SwiftSDKTests.swift
@@ -6,36 +6,28 @@
 //  Copyright © 2016 Knurld. All rights reserved.
 //
 
-import XCTest
+import Quick
+import Nimble
 import SwiftSDK
 
-class SwiftSDKTests: XCTestCase {
+class SwiftSDKTests: QuickSpec {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
-        //@todo actually implement an asynchronous test; for now, this just verifies linking
-        authorize(clientID: TEST_CLIENT_ID, clientSecret: TEST_CLIENT_SECRET,
-                  onSuccess: { accessToken in print("Application got token: \(accessToken)") },
-                  onFailure: { error in print("Application got error: \(error)") })
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    override func spec() {
+        /*
+        describe("SwiftSDK") {
+            it("works") {
+                expect(true).to(beTrue())
+            }
         }
+        */
+        
+        var accessToken: String? = nil
+        
+        authorize(clientID: TEST_CLIENT_ID, clientSecret: TEST_CLIENT_SECRET,
+                  onSuccess: { token in accessToken = token },
+                  onFailure: { error in print("Error: \(error)") })
+        
+        expect(accessToken).toEventuallyNot(beNil())
     }
-    
+ 
 }
